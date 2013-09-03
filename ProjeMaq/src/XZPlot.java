9b6aa41aac70dc246ebbdda79c9c6eb2c491bef6
aviggiano <antonio.viggiano@usp.br>
2013-09-03 00:59:06 -0300

    Plotar grafico XZ com a biblioteca JFreeChart

---
+++ ProjeMaq/src/XZPlot.java
@@ -0,0 +1,41 @@
+import javax.swing.JPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+
+public class XZPlot extends JPanel {
+
+    public XZPlot(String title) {
+
+        XYSeries series = new XYSeries("Random Data");
+        series.add(1.0, 500.2);
+        series.add(5.0, 694.1);
+        series.add(4.0, 100.0);
+        series.add(12.5, 734.4);
+        series.add(17.3, 453.2);
+        series.add(21.2, 500.2);
+        series.add(21.9, null);
+        series.add(25.6, 734.4);
+        series.add(30.0, 453.2);
+        XYSeriesCollection data = new XYSeriesCollection(series);
+        JFreeChart chart = ChartFactory.createXYLineChart(
+            "XZ Series Demo",
+            "X", 
+            "Z", 
+            data,
+            PlotOrientation.VERTICAL,
+            true,
+            true,
+            false
+        );
+
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(400, 300));
+        this.add(chartPanel);
+    }
+}
