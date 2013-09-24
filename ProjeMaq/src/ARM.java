0862956c946783770639d7c1763f1943de296247
aviggiano <antonio.viggiano@usp.br>
2013-09-24 00:00:23 -0300

    changes

--- ProjeMaq/src/ARM.java
+++ ProjeMaq/src/ARM.java
@@ -1,14 +1,13 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 
 public class ARM {
-	//lalalala
-	//lalalala
+
 	//lalalala
 	public ArrayList<String> readData (File f) {
 		GParser gParser = new GParser();
 		return gParser.readData(f);
 	}
+
 }
