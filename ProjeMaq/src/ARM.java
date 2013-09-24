5e014ef939b2fba5df5aaf7f74e55a5048e05f29
aviggiano <antonio.viggiano@usp.br>
2013-09-24 00:02:49 -0300

    z

--- ProjeMaq/src/ARM.java
+++ ProjeMaq/src/ARM.java
@@ -3,9 +3,6 @@ import java.util.ArrayList;
 
 
 public class ARM {
-	//lalalala
-
-	//lalalala
 	public ArrayList<String> readData (File f) {
 		GParser gParser = new GParser();
 		return gParser.readData(f);
